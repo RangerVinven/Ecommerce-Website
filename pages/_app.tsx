21aa52f5d75be6af928838b734198f2f10c24340
Daniel McPherson <daniel.mcpherson@live.co.uk>
2023-04-14 11:40:19 +0100

    Initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,26 @@
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
+// The below code is taken from https://dev.to/hpouyanmehr/nextjs-mui-v5-tutorial-2k35
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import React from 'react';
+import { CacheProvider } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+
+import createEmotionCache from '../utility/createEmotionCache';
+import lightTheme from '../styles/theme/lightTheme';
+import '../styles/globals.css';
+
+const clientSideEmotionCache = createEmotionCache();
+
+const MyApp = (props: any) => {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
+};
+
+export default MyApp;
